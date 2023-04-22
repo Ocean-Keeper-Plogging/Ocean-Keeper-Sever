a5ffa09ae0b62cb83bac2a4e795dfc5715224665
dev1week <rondo2860@gmail.com>
2023-04-22 14:12:41 +0900

    #5 -4 API Swagger 시험적용
    - swagger ui 상에서 부족한 정보를 채우기 위해 시험 적용 나중에 추가 수정할 예정

--- src/main/java/com/server/oceankeeper/Handler/CustomExceptionHandler.java
+++ src/main/java/com/server/oceankeeper/Handler/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.server.oceankeeper.Handler;
 
 import com.server.oceankeeper.DTO.ResponseDto;
 import com.server.oceankeeper.Exception.CustomApiException;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,5 +20,11 @@ public class CustomExceptionHandler {
     public ResponseEntity<?> apiException(CustomApiException e){
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeException(RuntimeException e){
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto(-1, e.getMessage(),null), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
