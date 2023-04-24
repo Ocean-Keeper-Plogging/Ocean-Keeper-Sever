9543a1f9074aafb656453e476018ef7fc7e5afc4
dev1week <rondo2860@gmail.com>
2023-04-25 00:15:37 +0900

    #9 -2 S3Service 이미지 삭제로직 수정
    1. removeS3File
    filePullPath를 넣어야 로직이 성립함 
    하지만 DB user테이블에서 바로 값을 꺼내기 때문에 fileFullPath가 아니라 url이 넘어옴
    때문에 도메인 주소를 파싱하는 로직 추가 
    2. revmove 메서드 수정 
    마찬가지로 AwsS3 Dto를 인자로 받는 것이 아닌 문자열 fileFullPath를 직접 넘겨주는 것으로 수정함

--- src/main/java/com/server/oceankeeper/Util/S3Service.java
+++ src/main/java/com/server/oceankeeper/Util/S3Service.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.server.oceankeeper.Exception.CustomApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,36 +14,40 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class AwsS3Service {
+public class S3Service {
 
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public AwsS3 multipartUpload(MultipartFile multipartFile, String dirName) throws IOException {
+    public ProfileReqDto multipartUpload(MultipartFile multipartFile, String dirName) throws IOException {
         File file = convertMultipartFileToFile(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File convert fail"));
 
         return upload(file, dirName);
     }
 
-    public AwsS3 upload(File file, String dirName) {
+    public ProfileReqDto upload(File file, String dirName) {
         String fileFullPath = randomFileName(file, dirName);
         String url = putS3(file, fileFullPath);
         removeFile(file);
 
-        return AwsS3
+        return ProfileReqDto
                 .builder()
                 .fileFullPath(fileFullPath)
                 .url(url)
                 .build();
     }
+
+
 
     private String randomFileName(File file, String dirName) {
         return dirName + "/" + UUID.randomUUID() + file.getName();
@@ -58,10 +63,15 @@ public class AwsS3Service {
         return amazonS3.getUrl(bucket, fileName).toString();
     }
 
-    public void removeS3File(String fileName){
-        final DeleteObjectRequest deleteObjectRequest
-                = new DeleteObjectRequest(bucket, fileName);
-        amazonS3.deleteObject(deleteObjectRequest);
+    private String getFileFullPath(String fileUrl){
+        String decodedFileUrl = null;
+        try {
+            decodedFileUrl = URLDecoder.decode(fileUrl, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new CustomApiException("file의 url이 유효하지 않습니다.");
+        }
+        String[] pathParts = decodedFileUrl.split("/");
+        return pathParts[pathParts.length - 2]+pathParts[pathParts.length - 1];
     }
 
     private void removeFile(File file) {
@@ -80,10 +90,14 @@ public class AwsS3Service {
         return Optional.empty();
     }
 
-    public void remove(AwsS3 awsS3) {
-        if (!amazonS3.doesObjectExist(bucket, awsS3.getFileFullPath())) {
-            throw new AmazonS3Exception("Object " +awsS3.getFileFullPath()+ " does not exist!");
+    public void remove(String fileFullPath) {
+        if (!amazonS3.doesObjectExist(bucket, fileFullPath)) {
+            throw new AmazonS3Exception("Object " + fileFullPath+ " does not exist!");
         }
-        amazonS3.deleteObject(bucket, awsS3.getFileFullPath());
+        amazonS3.deleteObject(bucket, fileFullPath);
+
+
     }
+
+
 }
